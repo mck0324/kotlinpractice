01fd3d62cb60f69f543c2cd833954d81ba05f551
mck0324 <dch3030@naver.com>
2023-08-16 16:58:35 +0900

    id로 user가져오기

--- src/main/kotlin/com/example/demo/users/UserController.kt
+++ src/main/kotlin/com/example/demo/users/UserController.kt
@@ -13,13 +13,26 @@ class UserController(
 ) {
 
     //get all users
+    @GetMapping("")
     fun getAllusers(): List<User> =
             userRepository.findAll().toList()
 
     //create user
+    @PostMapping("")
     fun createUser(@RequestBody user: User) : ResponseEntity<User> {
         val savedUser = userRepository.save(user)
         return ResponseEntity(savedUser, HttpStatus.CREATED)
+    }
+
+    //get user by id
+    @GetMapping("/{id}")
+    fun getUserById(@PathVariable("id") userId: Int) : ResponseEntity<User> {
+        val user = userRepository.findById(userId)
+        return if (user.isPresent) {
+            ResponseEntity(user.get(), HttpStatus.OK)
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
 
 }
