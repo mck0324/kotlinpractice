89a3349a618b22556151a2f02992d3d4b22c1885
mck0324 <dch3030@naver.com>
2023-08-16 16:52:59 +0900

    first commit

---
+++ src/main/kotlin/com/example/demo/users/UserController.kt
@@ -0,0 +1,23 @@
+package com.example.demo.users
+
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/users")
+class UserController(
+        @Autowired private val userRepository: UserRepository
+) {
+
+    //get all users
+    fun getAllusers(): List<User> =
+            userRepository.findAll().toList()
+
+    //create user
+    fun createUser(@RequestBody user: User) : ResponseEntity<User> {
+        ResponseEntity(userRepository.save(user), HttpStatus.CREATED)
+    }
+}
